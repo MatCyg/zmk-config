38848d4715a859e348c9877490a6c6df2d4abdc3
Mateusz Cygert <matcyg@gmail.com>
2021-12-14 02:54:59 +0100

    Refactor behaviors and custom keys to separate file

---
+++ config/keys.h
@@ -0,0 +1,8 @@
+#define _UNDO     LG(Z)
+#define _CUT      LG(X)
+#define _COPY     LG(C)
+#define _PASTE    LG(V)
+
+#define HYPER(key) LS(LC(LA(LG(key))))
+#define MEH(key)   LS(LC(LA(key)))
+#define HEM(key)   LG(LC(LA(key)))
