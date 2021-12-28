6498f696c5b332315c12534359abe2fd5ee90b5a
Mateusz Cygert <matcyg@gmail.com>
2021-12-28 11:29:00 +0100

    Fix shortcuts for adjust and function layers

--- config/keys.h
+++ config/keys.h
@@ -17,16 +17,16 @@
 #define _ZOOM_IN    LG(EQUAL)
 #define _LOCK       LG(LC(Q))
 
-#define _SS_CLIP    LG(LC(LS(Q)))
+#define _SS_CLIP    LG(LC(LS(N4)))
 //#define _SS_TOOL    LG(LC(Q))
 #define _TT_START   HYPER(H)
 #define _TT_SHOW    HYPER(SCLN)
 
 
 //adjust layer
-#define _LAY_BAR    LG(MINUS)
-#define _OS_DARK    LG(MINUS)
-#define _OS_LIGHT   LG(EQUAL)
-#define _TOG_DIACT  LG(EQUAL)
+#define _LAY_BAR    HYPER(BSLH)
+#define _OS_LIGHT   HYPER(LBKT)
+#define _OS_DARK    HYPER(RBKT)
+#define _TOG_DIACT  HYPER(SQT)
 
 
